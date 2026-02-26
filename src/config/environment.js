367b13ae706ced0be3a0b2fafe17c925bf2698b2
GuidoAlferez <alferezguido@outlook.com>
2026-02-26 03:36:33 -0300

    se hace un primer commit

---
+++ src/config/environment.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const ENVIRONMENT = {
+    MONGO_DB_CONNECTION_STRING: process.env.MONGO_DB_CONNECTION_STRING,
+    PORT : process.env.PORT,
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY
+}
+export default ENVIRONMENT
